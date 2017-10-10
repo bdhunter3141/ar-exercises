97d33ad23cdaed836d110007712e88dda584d680
Bryan Hunter <bdhunter3141@gmail.com>
2017-10-10 20:20:45 +0000

    Completed last exercise.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Please enter a store name: > "
+@store_name = $stdin.gets.chomp
+@new_store = Store.new(name: @store_name, annual_revenue: -1)
+@new_store.save
+if @new_store.errors.any?
+  @new_store.errors.each do |attribute, message|
+    puts "Error: #{attribute} #{message}"
+  end
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true,  greater_than_or_equal_to: 0 }
+  validate :carries_clothes
+
+  def carries_clothes
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "can't have both apparels false")
+      errors.add(:womens_apparel, "can't have both apparels false")
+    end
+  end
 end
